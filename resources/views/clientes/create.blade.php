f62b39844b6d2ffe830c0d326a218e42657f56b5
Elton Fonseca <elton869@gmail.com>
2024-02-16 13:43:37 -0300

    10 - Criando componentes para melhorar a legibilidade dos formulários

--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -1,19 +1,10 @@
 <x-layout titulo="Cadastrar novo Cliente">
     <form method="post" action="{{ route('clientes.store') }}" class="max-w-6xl mx-auto">
         @csrf
-        <div class="mb-5">
-            <label for="nome" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome do cliente</label>
-            <input type="text" value="{{ old('nome') }}" id="nome" name="nome" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light">
-        </div>
-        <div class="mb-5">
-            <label for="endereco" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Endereço do cliente</label>
-            <input type="text" value="{{ old('endereco') }}" id="endereco" name="endereco" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light">
-        </div>
-        <div class="mb-5">
-            <label for="descricao" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descrição do cliente</label>
-            <input type="text" value="{{ old('descricao') }}" id="descricao" name="descricao" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light">
-        </div>
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cadastrar Cliente</button>
+        <x-input nome="nome" labelTitulo="Nome do cliente" />
+        <x-input nome="endereco" labelTitulo="Endereço do cliente" />
+        <x-input nome="descricao" labelTitulo="Descrição do cliente" />
+        <x-botao-primario titulo="Cadastrar Cliente" />
     </form>
 </x-layout>
 
---
+++ resources/views/components/label.blade.php
@@ -0,0 +1,1 @@
+<label {{ $attributes }} class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ $titulo }}</label>
