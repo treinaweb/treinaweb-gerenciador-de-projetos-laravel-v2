c200b4594ee6e29eea0e2149fbb830cd41bae47e
Elton Fonseca <elton869@gmail.com>
2024-02-20 15:37:57 -0300

    34 - Adicionando o cadastro de funcionários ao menu

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,17 +6,7 @@
         <script src="https://cdn.tailwindcss.com"></script>
     </head>
     <body>
-        <nav class="bg-gray-300">
-            <div class="container mx-auto flex items-center justify-between p-4">
-                <a href="/" class="text-2xl font-semibold">Treinaweb</a>
-            
-                <ul class="font-medium flex">
-                    <li class="px-4">
-                        <a href="{{ route('clientes.index') }}">Cadastro de Clientes</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <x-menu />
 
         <div class="container mx-auto">
             <h1 class="text-4xl font-bold text-center my-4">
---
+++ resources/views/components/menu.blade.php
@@ -0,0 +1,14 @@
+<nav class="bg-gray-300">
+    <div class="container mx-auto flex items-center justify-between p-4">
+        <a href="/" class="text-2xl font-semibold">Treinaweb</a>
+    
+        <ul class="font-medium flex">
+            <li class="px-4">
+                <a href="{{ route('clientes.index') }}">Clientes</a>
+            </li>
+            <li class="px-4">
+                <a href="{{ route('funcionarios.index') }}">Funcionários</a>
+            </li>
+        </ul>
+    </div>
+</nav>
