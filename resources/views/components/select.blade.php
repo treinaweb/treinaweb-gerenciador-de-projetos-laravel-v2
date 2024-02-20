26f669ccbcfbaff55a232d32651c83629182ba07
Elton Fonseca <elton869@gmail.com>
2024-02-20 13:51:59 -0300

    30 - Mostrando os estados no cadastro de funcionário

---
+++ resources/views/components/select.blade.php
@@ -0,0 +1,22 @@
+@php
+    $valorPadrao = old($nome, $valorPadrao ?? '');
+@endphp
+
+<div class="mb-5">
+    <x-label for="{{ $nome }}" titulo="{{ $labelTitulo }}" />
+    <select
+        id="{{ $nome }}"
+        name="{{ $nome }}"
+        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+    >
+        <option>Selecione um item</option>
+        @foreach ($lista as $item)
+            <option
+                value="{{ $item->name }}"
+                {{ $item->name == $valorPadrao ? 'selected' : ''  }}
+            >
+                {{ $item->value }}
+            </option>
+        @endforeach
+    </select>
+</div>
